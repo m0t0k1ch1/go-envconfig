7fe1a29789a6342ad9126c9b86c4aec2dddab433
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2021-02-18 11:04:07 +0900

    [internal/testutils] update

--- internal/testutils/helpers.go
+++ internal/testutils/helpers.go
@@ -1,14 +1,34 @@
 package testutils
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
-// Equal checks if actual and expected are equal.
-func Equal(t *testing.T, actual, expected interface{}) {
-	if diff := cmp.Diff(actual, expected); diff != "" {
+// Equal checks if want and got are equal.
+func Equal(t *testing.T, want, got interface{}) {
+	t.Helper()
+
+	if diff := cmp.Diff(want, got); diff != "" {
 		t.Errorf("diff: %s", diff)
+	}
+}
+
+// TestErrorMessage checks if err message contains s.
+func TestErrorMessage(t *testing.T, s string, err error) {
+	t.Helper()
+
+	if s == "" {
+		if err != nil {
+			t.Errorf("err is not nil: %v", err)
+			return
+		}
+		return
+	}
+
+	if contained := strings.Contains(err.Error(), s); !contained {
+		t.Errorf(`err message does not contain "%s": %v`, s, err)
 	}
 }
