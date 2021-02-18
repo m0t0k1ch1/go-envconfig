98bd56ddca4b05365d0bde3802fd747691d6d8d7
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2021-02-18 11:05:20 +0900

    add tests

--- parse_test.go
+++ parse_test.go
@@ -1,7 +1,11 @@
 package envparser
 
 import (
+	"fmt"
+	"math"
+	"math/bits"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/m0t0k1ch1/go-envparser/internal/testutils"
@@ -11,15 +15,65 @@ const (
 	testEnvKey = "GO_ENVPARSER_TEST"
 )
 
+func TestParseInvalidArgError(t *testing.T) {
+	var err error
+
+	err = Parse(testEnvKey, nil)
+	testutils.TestErrorMessage(t, "v cannot be nil", err)
+
+	err = Parse(testEnvKey, string(""))
+	testutils.TestErrorMessage(t, "v cannot be non-pointer string", err)
+
+	err = Parse(testEnvKey, (*string)(nil))
+	testutils.TestErrorMessage(t, "v cannot be nil *string", err)
+}
+
 func TestParseAsString(t *testing.T) {
 	defer os.Clearenv()
 	os.Setenv(testEnvKey, "string")
 
 	var s string
-	if err := Parse(testEnvKey, &s); err != nil {
-		t.Error(err)
-		return
+	err := Parse(testEnvKey, &s)
+	testutils.TestErrorMessage(t, "", err)
+	testutils.Equal(t, "string", s)
+}
+
+func TestParseAsInt(t *testing.T) {
+	cases := []struct {
+		s   string
+		i   int
+		err string
+	}{{
+		s:   strconv.Itoa(-maxInt()),
+		i:   -maxInt(),
+		err: "",
+	}, {
+		s:   strconv.Itoa(maxInt()),
+		i:   maxInt(),
+		err: "",
+	}, {
+		s:   "zero",
+		i:   0,
+		err: fmt.Sprintf("cannot parse %s as int", testEnvKey),
+	}}
+
+	for _, c := range cases {
+		t.Run(c.s, func(t *testing.T) {
+			defer os.Clearenv()
+			os.Setenv(testEnvKey, c.s)
+
+			var i int
+			err := Parse(testEnvKey, &i)
+			testutils.TestErrorMessage(t, c.err, err)
+			testutils.Equal(t, c.i, i)
+		})
+	}
+}
+
+func maxInt() int {
+	if bits.UintSize == 32 {
+		return math.MaxInt32
 	}
 
-	testutils.Equal(t, s, "string")
+	return math.MaxInt64
 }
